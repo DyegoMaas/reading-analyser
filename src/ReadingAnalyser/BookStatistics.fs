8c8ad5ae34da2ed53b0a6a9c69567298c360c48f
Dyego <dyego.maas@gmail.com>
2016-03-06 18:48:06 -0300

    Adicionada função para calcular o percentual restante de um audiobook, dado o tempo já ouvido e o tempo restante.

---
+++ src/ReadingAnalyser/BookStatistics.fs
@@ -0,0 +1,16 @@
+﻿namespace ReadingAnalyser
+
+open System
+
+module BookStatistics =
+
+    module Audiobook =
+    
+        let GetRemainingPercentage (timeListened:TimeSpan, timeRemaining:TimeSpan) : double = 
+            let milisecondsCompleted = timeListened.TotalMilliseconds
+            let milisecondsRemaining = timeRemaining.TotalMilliseconds
+
+            let audiobookDuration = milisecondsCompleted + milisecondsRemaining
+            let percentageCompleted =  milisecondsCompleted / audiobookDuration * 100.0
+            percentageCompleted
+            
--- tests/ReadingAnalyser.Tests/Tests.fs
+++ tests/ReadingAnalyser.Tests/Tests.fs
@@ -1,10 +1,17 @@
 module ReadingAnalyser.Tests
 
-open ReadingAnalyser
+open ReadingAnalyser.BookStatistics
+open System
 open NUnit.Framework
+open FsUnit
 
 [<Test>]
-let ``hello returns 42`` () =
-  let result = Library.hello 42
-  printfn "%i" result
-  Assert.AreEqual(42,result)
+let ``getting 20% remaining from 2h listened and 8h remaining`` () =
+    let timeListened = TimeSpan.FromHours 2.0
+    let timeRemained = TimeSpan.FromHours 8.0
+    let remainingPercentage = Audiobook.GetRemainingPercentage 
+    
+    remainingPercentage |> should equal 20.0
+//    let result = Library.hello 42
+//    printfn "%i" result
+//    Assert.AreEqual(42,result)
