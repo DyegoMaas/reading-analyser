78fbd881a3cd2cc2c4388d4e1adb28e932bedead
Dyego <dyego.maas@gmail.com>
2016-03-06 18:17:36 -0300

    Criado projeto utilizando o ProjectScaffolding.

---
+++ src/ReadingAnalyser/AssemblyInfo.fs
@@ -0,0 +1,12 @@
+﻿namespace System
+open System.Reflection
+
+[<assembly: AssemblyTitleAttribute("ReadingAnalyser")>]
+[<assembly: AssemblyProductAttribute("ReadingAnalyser")>]
+[<assembly: AssemblyDescriptionAttribute("Analisa o progresso de leitura de livros e audiobooks")>]
+[<assembly: AssemblyVersionAttribute("1.0")>]
+[<assembly: AssemblyFileVersionAttribute("1.0")>]
+do ()
+
+module internal AssemblyVersionInformation =
+    let [<Literal>] Version = "1.0"
