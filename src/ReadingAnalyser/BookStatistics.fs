3858fdf4e2ba3df24f1d137ffef29b05e10b8bb1
Dyego <dyego.maas@gmail.com>
2016-03-07 00:29:16 -0300

    Atualizações para rodar com o NUnit3. Não consegui fazer funcionar o fluent FsUnit.

--- src/ReadingAnalyser/BookStatistics.fs
+++ src/ReadingAnalyser/BookStatistics.fs
@@ -6,11 +6,11 @@ module BookStatistics =
 
     module Audiobook =
     
-        let GetRemainingPercentage (timeListened:TimeSpan, timeRemaining:TimeSpan) : double = 
+        let GetRemainingPercentage (timeListened:TimeSpan, timeRemaining:TimeSpan) : float = 
             let milisecondsCompleted = timeListened.TotalMilliseconds
             let milisecondsRemaining = timeRemaining.TotalMilliseconds
 
             let audiobookDuration = milisecondsCompleted + milisecondsRemaining
             let percentageCompleted =  milisecondsCompleted / audiobookDuration * 100.0
-            percentageCompleted
+            float percentageCompleted
             
--- tests/ReadingAnalyser.Tests/Tests.fs
+++ tests/ReadingAnalyser.Tests/Tests.fs
@@ -9,9 +9,9 @@ open FsUnit
 let ``getting 20% remaining from 2h listened and 8h remaining`` () =
     let timeListened = TimeSpan.FromHours 2.0
     let timeRemained = TimeSpan.FromHours 8.0
-    let remainingPercentage = Audiobook.GetRemainingPercentage 
     
-    remainingPercentage |> should equal 20.0
-//    let result = Library.hello 42
-//    printfn "%i" result
-//    Assert.AreEqual(42,result)
+    let remainingPercentage = Audiobook.GetRemainingPercentage(timeListened, timeRemained)
+    
+    Assert.AreEqual(20.0, remainingPercentage)
+
+//    Assert.AreEqual(2, 2)
